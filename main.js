897fa4f509e9f99c1f5ae539fc9860b51a770170
Muhammad Talha <141347189+MuhammadTalha15@users.noreply.github.com>
2023-12-03 18:57:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+let display = document.getElementById('disp');
+let btns = document.querySelectorAll('#btn');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        let value;
+        value = e.target.value;
+        display.value += value
+    })
+});
+
+const perform_valid_operation = ()=>{
+    const equal_btn = document.getElementById('equals');
+    
+    equal_btn.addEventListener('click', ()=>{
+        let result = eval(display.value);
+        display.value = result;
+    });
+}
+
+const clear_all = ()=>{
+    const clear_btn = document.getElementById('clear-all')
+
+    clear_btn.addEventListener('click', ()=>{
+        display.value = '';
+    });
+}
+
+const delete_char = ()=>{
+    const delete_char = document.getElementById('delete');
+
+    delete_char.addEventListener('click', ()=>{
+        let data = display.value;
+        display.value = data.slice(0, -1);
+    });
+}
+
+perform_valid_operation();
+clear_all();
+delete_char();
